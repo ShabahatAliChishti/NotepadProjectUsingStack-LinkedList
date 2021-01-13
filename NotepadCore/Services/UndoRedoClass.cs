2ccd821da1d5aefa1beab22eb88f111a6b6807ab
umernasir19 <umernasir19@gmail.com>
2021-01-13 14:13:54 +0500

    First commit

---
+++ NotepadCore/Services/UndoRedoClass.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NotepadCore.Services
+{
+    public class UndoRedoClass
+    {
+        private static Stack<string> UndoStack = new Stack<string>();
+        private static Stack<string> RedoStack = new Stack<string>();
+        public UndoRedoClass()
+        {
+            UndoStack = new Stack<string>();
+            RedoStack = new Stack<string>();
+        }
+
+        public void Clear()
+        {
+            UndoStack.Clear();
+            RedoStack.Clear();
+        }
+
+        public void AddItem(string item)
+        {
+            UndoStack.Push(item);
+        }
+
+        public string Undo()
+        {
+            string final = string.Empty;
+            if (UndoStack.Count() > 0)
+            {
+                string item = UndoStack.Pop();
+
+                RedoStack.Push(item);
+
+
+
+                string removecomma = item.Remove(item.Length - 1);
+
+                if (removecomma.Contains(" ") || removecomma != "")
+                {
+                    final = removecomma;
+                }
+                EditOperation.txtAreaTextChangeRequired = true;
+                return final;
+
+            }
+            else
+            {
+                EditOperation.txtAreaTextChangeRequired = true;
+
+                return final;
+            }
+
+        }
+
+
+        public string Redo()
+        {
+            string final = string.Empty;
+
+            if (RedoStack.Count() > 0)
+            {
+                //if (RedoStack.Count == 0)
+                //return UndoStack.First();
+                string item = RedoStack.Pop();
+                UndoStack.Push(item);
+
+                EditOperation.txtAreaTextChangeRequired = true;
+                return UndoStack.First();
+            }
+            else
+            {
+                EditOperation.txtAreaTextChangeRequired = true;
+
+                return final;
+            }
+        }
+
+        public bool CanUndo()
+        {
+            return UndoStack.Count > 1;
+        }
+
+        public bool CanRedo()
+        {
+            return RedoStack.Count >=1;
+        }
+
+        public List<string> UndoItems()
+        {
+            return UndoStack.ToList();
+        }
+
+        public List<string> RedoItems()
+        {
+            return RedoStack.ToList();
+        }
+    }
+}
---
+++ NotepadProject/Forms/FormAbout.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace NotepadProject.Forms
+{
+    partial class FormAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Developed by Hareem Ali Malik And Irtaza Zafar";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYear.Location = new System.Drawing.Point(104, 35);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(35, 13);
+            this.lblYear.TabIndex = 1;
+            this.lblYear.Text = "2021";
+            // 
+            // FormAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 79);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormAbout";
+            this.ShowInTaskbar = false;
+            this.Text = "About Notepad";
+            this.Load += new System.EventHandler(this.FormAbout_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblYear;
+    }
+}
